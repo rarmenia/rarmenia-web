7a4f0b66c6cf131e8614326a0ce86d5091d97aab
rarmenia <rei.armenia@gmail.com>
2021-11-08 13:34:07 -0500

    intialization of project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from "./core/layout/Layout";
+
+function App() {
+  return (
+    <div className="content-fill">
+      <Layout>
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/core/layout/Layout.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+interface Props {
+}
+
+const Layout = (props: Props) => {
+    return(
+        <div>
+            layout
+        </div>
+    )
+}
+
+export default Layout
