a49f7ac24e265c6c072b8e5cf57bb69c44eb16ce
rarmenia <rei.armenia@gmail.com>
2022-01-05 14:11:46 -0500

    begin landing page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   const layout = useSelector((state: AppState) => state.layout)
 
-  return (<div className="flex flex-col h-full w-screen">
+  return (<div className="flex flex-col h-full w-full min-h-full min-w-full">
     {layout.showHeader && (<div className="flex flex-row bg-orange-800 text-amber-100 w-screen h-12 items-center shadow-slate-300 shadow-sm">
       <h1 className="text-xl ml-3.5">Rei Armenia</h1>
     </div>)}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type {NextPage} from 'next';
+import Head from 'next/head';
 import {useDispatch} from 'react-redux';
 import {useEffect} from 'react';
 import {setLayout} from '../redux/layout/actions';
@@ -9,17 +9,43 @@ const Home: NextPage = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(setLayout({showHeader: false}))
-  }, [dispatch])
+    dispatch(setLayout({showHeader: false}));
+  }, [dispatch]);
 
   return (<>
     <Head>
       <title>Rei Armenia ⎔ Front-End Developer</title>
     </Head>
-    <main className={'h-screen w-screen min-h-screen min-w-screen'}>
-      <p>landing page</p>
+    <main className={'w-screen h-screen flex items-center justify-center bg-stone-300 overflow-hidden'}>
+      <div className={'w-full h-full md:w-2/3 md:h-auto'}>
+        <div className={'h-full w-full min-w-full min-h-full bg-stone-100 shadow-stone-400 shadow-md drop-shadow flex flex-col overflow-auto'}>
+          <div className={'flex flex-col items-center justify-center my-4'}>
+            <h1 className={'text-2xl text-stone-800'}>Rei Armenia</h1>
+            <div className={'mt-[-.5rem]'} role={'doc-subtitle'}>
+              <span className={'text-[.75rem] text-stone-700'}>
+                Full-Stack Web Developer&nbsp;
+                <strong className={'text-stone-800 font-semibold'}>[Front End Focused]</strong>
+              </span>
+            </div>
+          </div>
+          <div className={'flex flex-col items-center justify-center my-8'}>
+            <div className={'bg-amber-600 w-44 h-44 rounded-full'} />
+          </div>
+          <div className={'flex-grow'} />
+          <div className={'flex flex-col items-center justify-center my-4 justify-self-end'}>
+            <div className={'text-[.75rem]'}>
+              New York State Department of Information Technology Services
+            </div>
+            <div className={'text-[.75rem]'}>
+              <strong className={'text-stone-800 font-semibold'}>
+                Information Technology Specialist 2 (Programming)
+              </strong>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
-  </>)
-}
+  </>);
+};
 
-export default Home
+export default Home;
