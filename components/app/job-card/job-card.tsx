7d625a8c943c51b3af8e9d7d4af103d7d1282c15
Rei Armenia <anthony.armenia@its.ny.gov>
2022-01-18 13:10:31 -0500

    add folding to duties

--- components/app/job-card/job-card.tsx
+++ components/app/job-card/job-card.tsx
@@ -67,10 +67,10 @@ export default function JobCard(props: Props): JSX.Element {
                 </Typography>
               </Disclosure.Button>
               <Transition
-                enter="transition duration-100 ease-out"
+                enter="transition duration-200 ease-out"
                 enterFrom="transform scale-95 opacity-0"
                 enterTo="transform scale-100 opacity-100"
-                leave="transition duration-75 ease-out"
+                leave="transition duration-150 ease-out"
                 leaveFrom="transform scale-100 opacity-100"
                 leaveTo="transform scale-95 opacity-0"
               >
--- components/app/project-card/project-info.tsx
+++ components/app/project-card/project-info.tsx
@@ -1,4 +1,6 @@
-import {Project} from '../../../interfaces/project';
+import { Disclosure, Transition } from '@headlessui/react';
+import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/solid';
+import { Project } from '../../../interfaces/project';
 import Typography from '../../generics/typography';
 
 interface Props {
@@ -11,16 +13,16 @@ export default function ProjectInfo(props: Props): JSX.Element {
       <div className={'flex flex-col text-left'}>
         <div className={'flex flex-col'}>
           <Typography type={'subheading-2'} text={props.project.name ?? ''}
-                      style={{weight: 'semibold', className: 'text-[.925rem]'}}/>
+            style={{ weight: 'semibold', className: 'text-[.925rem]' }} />
           {(props.project.timeframe?.start || props.project.timeframe.end) && (
-            <Typography type={'paragraph'} style={{weight: 'thin', className: 'text-sm mt-[-.25rem] text-stone-700'}}>
+            <Typography type={'paragraph'} style={{ weight: 'thin', className: 'text-sm mt-[-.25rem] text-stone-700' }}>
               <span>{props.project.timeframe?.start?.toDate().toLocaleDateString() ?? '???'}</span>
               <span> - </span>
               <span>{props.project.timeframe?.end?.toDate().toLocaleDateString() ?? 'Ongoing'}</span>
             </Typography>
           )}
           {props.project.role && (
-            <Typography type={'paragraph'} style={{weight: 'semibold', className: 'text-[.85rem] text-stone-900'}}>
+            <Typography type={'paragraph'} style={{ weight: 'semibold', className: 'text-[.85rem] text-stone-900' }}>
               <span>Role: </span>
               <span className={'text-amber-700'}>{props.project.role ?? ''}</span>
             </Typography>
@@ -28,7 +30,7 @@ export default function ProjectInfo(props: Props): JSX.Element {
           {(props.project.technologies?.length ?? 0) > 0 && (<div className={'flex-row flex gap-1 flex-wrap'}>
             {props.project.technologies.map((tech, index) => {
               return (<div key={index}>
-                <Typography type={'text'} text={tech} style={{weight: 'thin', className: 'text-sm text-stone-600'}}/>
+                <Typography type={'text'} text={tech} style={{ weight: 'thin', className: 'text-sm text-stone-600' }} />
                 {(index + 1 < props.project.technologies.length) && (
                   <span className="text-sm text-stone-600"> •</span>)}
               </div>);
@@ -36,22 +38,61 @@ export default function ProjectInfo(props: Props): JSX.Element {
           </div>)}
         </div>
         <div className={'flex flex-col mt-4'}>
-          <Typography type={'paragraph'} style={{weight: 'thin', className: 'text-[.9rem]'}}>
+          <Typography type={'paragraph'} style={{ weight: 'thin', className: 'text-[.9rem]' }}>
             {props.project.description}
           </Typography>
         </div>
-        {props.project.duties.length > 0 && (
-          <div className={'flex flex-col mt-2'}>
+        {(props.project.duties?.length ?? 0) > 0 && (<>
+          <Disclosure defaultOpen={false}>
+            {({ open }) => (
+              <>
+                <Disclosure.Button className={'pt-4 h-auto text-left'}>
+                  <Typography type={'subheading'} style={{ weight: 'semibold', className: 'text-sm' }}>
+                    <span>
+                      <span>Project </span>
+                      <span className={'text-amber-700'}>Duties</span>
+                      <span>
+                        {open ? <ChevronUpIcon className={'h-8 inline mb-[.125rem]'} /> :
+                          <ChevronDownIcon className={'h-8 inline mb-[.125rem]'} />}
+                      </span>
+                    </span>
+                  </Typography>
+                </Disclosure.Button>
+                <Transition
+                  enter="transition duration-100 ease-out"
+                  enterFrom="transform scale-95 opacity-0"
+                  enterTo="transform scale-100 opacity-100"
+                  leave="transition duration-75 ease-out"
+                  leaveFrom="transform scale-100 opacity-100"
+                  leaveTo="transform scale-95 opacity-0"
+                >
+                  <Disclosure.Panel>
+                    <div className={'flex flex-col items-start justify-start md:ml-8'}>
+                      <ul className={'list-disc ml-4 text-[.925rem] text-stone-800 font-light'}>
+                        {(props.project.duties ?? []).map((duty, index) => {
+                          return (
+                            <li key={index}><Typography type={'text'} text={duty} style={{ className: 'py-2' }} /></li>
+                          );
+                        })}
+                      </ul>
+                    </div>
+                  </Disclosure.Panel>
+                </Transition>
+              </>
+            )}
+          </Disclosure>
+          {/* <div className={'flex flex-col mt-2'}>
             <Typography type={'paragraph'} text={'On this project, I:'}
-                        style={{weight: 'semibold', className: 'text-[.9rem]'}}/>
+              style={{ weight: 'semibold', className: 'text-[.9rem]' }} />
             <ul className={'list-disc ml-4 text-[.925rem] text-stone-800 font-light'}>
               {(props.project.duties ?? []).map((duty, index) => {
                 return (
-                  <li key={index}><Typography type={'text'} text={duty} style={{className: 'py-2'}}/></li>
+                  <li key={index}><Typography type={'text'} text={duty} style={{ className: 'py-2' }} /></li>
                 );
               })}
             </ul>
-          </div>
+          </div> */}
+        </>
         )}
 
       </div>
