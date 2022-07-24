8c1135294caeb36dc555450ea769549c5d6d1202
Rei Armenia <67706744+reiarmenia@users.noreply.github.com>
2022-07-24 01:56:47 -0400

    nextjs + tailwind base project

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div className='bg-amber-600 text-amber-100'>
+        test
+    </div> 
+  )
+}
+
+export default Home
