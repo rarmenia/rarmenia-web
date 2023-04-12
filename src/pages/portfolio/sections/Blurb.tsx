8fb07017e5cc39666083a003ebacb115edce7006
Rei Armenia <5091445+rarmenia@users.noreply.github.com>
2023-04-12 10:59:40 -0400

    shadow

--- src/pages/portfolio/sections/Blurb.tsx
+++ src/pages/portfolio/sections/Blurb.tsx
@@ -7,7 +7,7 @@ const Blurb = () => {
     <div className="w-screen h-screen grid place-items-center p-2">
 
       <div className="relative isolate text-white w-10/12 md:w-2/4 font-mono">
-        <div className="z-0 bg-stone-300/20 backdrop-blur-lg py-6 px-8 ml-6 md:ml-0 w-auto rounded-lg flex-col flex gap-2">
+        <div className="z-0 bg-stone-300/20 backdrop-blur-lg py-6 px-8 ml-6 md:ml-0 w-auto rounded-lg flex-col flex gap-2 shadow-sm shadow-slate-900">
           <p>
             Hey there!
           </p>
@@ -38,7 +38,7 @@ const Blurb = () => {
             Plus, all of that trivia knowledge comes in handy on game nights!
           </p>
         </div>
-        <div className="absolute z-10 -left-8 md:-left-14 -bottom-4 bg-stone-300/20 backdrop-blur-lg rounded-full p-2" aria-hidden="true">
+        <div className="absolute z-10 -left-8 md:-left-14 -bottom-4 bg-stone-300/20 backdrop-blur-lg rounded-full p-2 shadow-sm shadow-slate-900" aria-hidden="true">
           <ChatBubbleOvalLeftEllipsisIcon className="h-8 -mt-0.5" />
         </div>
       </div>
