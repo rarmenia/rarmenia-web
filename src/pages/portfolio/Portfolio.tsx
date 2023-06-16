36ec63f5a27a56d465f708e2d6fbcd687ce3c561
Rei Armenia <5091445+rarmenia@users.noreply.github.com>
2023-06-16 09:14:24 -0400

    remove work reference so push can happen

--- src/pages/portfolio/Portfolio.tsx
+++ src/pages/portfolio/Portfolio.tsx
@@ -9,44 +9,44 @@ import Work from './sections/Work/Work';
 import { Me } from '@data/me';
 
 const Portfolio = () => {
-
   return (
     <>
       <PortfolioTopBar />
-      <div className='relative'>
+      <div className="relative">
         <PortfolioBackground
-          video={vimeo_link(
-            '385101859',
-            { autoplay: 1, autopause: 0, muted: true, loop: 1, controls: 0, title: 0, byline: 0, portrait: 0, quality: '720p' }
-          )}
+          video={vimeo_link('385101859', {
+            autoplay: 1,
+            autopause: 0,
+            muted: true,
+            loop: 1,
+            controls: 0,
+            title: 0,
+            byline: 0,
+            portrait: 0,
+            quality: '720p',
+          })}
           aspect={1.9 / 1}
           multiplyContent={<Hero />}
-          img='/img/background-still.png'
+          img="/img/background-still.png"
         />
-        <div className='bg-transparent w-screen z-10 isolate py-2'>
+        <div className="bg-transparent w-screen z-10 isolate py-2">
           <main>
-
             {/* Ensure that HeroContent will appear in Screen Readers */}
-            <div className='sr-only'><HeroContent /></div>
-            {/* Offset one Full Screen -> function as  HERO */}
-            <div className='bg-transparent h-screen' />
-
-
-            <div className='flex flex-col gap-60 mb-20'>
-
-              <Info />
-              <Work workplaces={Me.professional.work} />
-
+            <div className="sr-only">
+              <HeroContent />
             </div>
+            {/* Offset one Full Screen -> function as  HERO */}
+            <div className="bg-transparent h-screen" />
 
-
-
+            <div className="flex flex-col gap-60 mb-20">
+              <Info />
+              {/* <Work workplaces={Me.professional.work} /> */}
+            </div>
           </main>
         </div>
       </div>
     </>
   );
 };
-
 
 export default Portfolio;
