9d413a61887180c707e49f5c2db32141e2a1fdb1
Rei Armenia <rei.armenia@gmail.com>
2023-08-01 18:24:22 -0400

    use proper layouts + bones

--- src/components/interactive/MenuItem.tsx
+++ src/components/interactive/MenuItem.tsx
@@ -14,7 +14,7 @@ const MenuItem = (props: MenuItemProps) => {
       onClick={() => {
         router.push(props.destination);
       }}
-      className={`p-4 md:w-40 md:pl-8 md:justify-start inline-flex flex-row justify-center items-center gap-2 border ${props.active ? 'bg-sky-500 text-sky-500 border-sky-300/10' : 'border-white/10 hover:bg-stone-50/5 text-white'} bg-opacity-5 rounded-sm`}
+      className={`p-4 lg:w-40 lg:pl-8 lg:justify-start inline-flex flex-row justify-center items-center gap-2 border ${props.active ? 'bg-sky-500 text-sky-500 border-sky-300/10' : 'border-white/10 hover:bg-stone-50/5 text-white'} bg-opacity-5 rounded-sm`}
     >
       {props.icon}
       <div className="text-white font-sans hidden md:block font-light">{props.label}</div>
