03fad295f0119dcd4432cd18bb6a947fdaee5ccb
Rei Armenia <rei.armenia@gmail.com>
2023-08-08 13:54:40 -0400

    skills section

--- src/components/display/skill/SkillAreaCard.tsx
+++ src/components/display/skill/SkillAreaCard.tsx
@@ -1,30 +1,37 @@
 import GlassCard from "@/components/containers/GlassCard"
+import { SkillAreaStorage } from "@/types/skill/skill-area";
 import { SkillAreaIconType } from "@/types/skill/skill-area-icon-type";
 import { CloudIcon, ComputerDesktopIcon, CpuChipIcon, RectangleGroupIcon, UserGroupIcon } from "@heroicons/react/20/solid"
+import KeyExperience from "../information/KeyExperience";
 
 const ICON_MAP: { [key in SkillAreaIconType]: React.ReactNode } = {
-  'people': <UserGroupIcon />,
-  'monitor': <ComputerDesktopIcon />,
-  'cpu': <CpuChipIcon />,
-  'cloud': <CloudIcon />,
-  'rects': <RectangleGroupIcon />
-}
+  'people': <UserGroupIcon className="h-6" />,
+  'monitor': <ComputerDesktopIcon className="h-6" />,
+  'cpu': <CpuChipIcon className="h-6" />,
+  'cloud': <CloudIcon className="h-6" />,
+  'rects': <RectangleGroupIcon className="" />
+} as const;
 
 type SkillAreaCardProps = {
-  icon: SkillAreaIconType;
-
+  skill: SkillAreaStorage
 }
 
-const SkillAreaCard = (props: SkillAreaCardProps) => {
-  const icon = ICON_MAP[props.icon];
+const SkillAreaCard = ({ skill }: SkillAreaCardProps) => {
   return (
     <GlassCard>
-      <div className="flex flex-col gap-2 items-center p-2">
-        <div className="flex flex-row gap-1">
-          <div>{icon}</div>
+      <div className="w-[22rem] h-52 p-2 flex flex-col items-center gap-2 justify-evenly">
+        <div className="flex flex-col gap-2 items-center">
+          <div className="flex flex-row gap-2">
+            {ICON_MAP[skill.icon]}
+            <div> {skill.name} </div>
+          </div>
+          <KeyExperience display={skill.key} />
         </div>
+        <ul className="w-full grid grid-cols-2 items-center px-4 gap-4">
+          {skill.other.map((single, ind) => <li key={ind} className="text-xs text-white/60 font-mono col-span-1">{single}</li>)}
+        </ul>
       </div>
-    </GlassCard>
+    </GlassCard >
   )
 }
 
