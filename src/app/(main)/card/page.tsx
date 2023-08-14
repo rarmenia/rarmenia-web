cce5c40ab53cbce640c288d84ccda6fdfea46eb2
Rei Armenia <5091445+rarmenia@users.noreply.github.com>
2023-08-13 21:17:35 -0400

    finish grid

--- src/app/(main)/card/page.tsx
+++ src/app/(main)/card/page.tsx
@@ -1,24 +1,14 @@
-import GlassCard from "@/components/containers/GlassCard";
-import Location from "@/components/display/information/Location";
+import BusinessCard from "@/components/display/card-contact/BusinessCard";
 
 const VirtualBusinessCard = (
 ) => {
-  return <div className="h-[90vh] grid place-items-center">
-    <div className="w-fit">
-      <GlassCard>
-        <div className="min-w-[22rem] flex flex-col items-center p-4 gap-4">
-          <div className="h-56 aspect-square bg-stone-700 grid place-items-center overflow-hidden rounded-full"></div>
-          <div className="flex flex-col items-center gap-1">
-            <div className="flex flex-row items-center gap-2">
-              <div className="text-xl font-sans font-semibold text-white">Rei Armenia</div>
-              <div className="italic text-sm text-white/40">{'(they/them)'}</div>
-            </div>
-            <Location location={'Capital Region, New York, USA'} />
-          </div>
-        </div>
-      </GlassCard>
-    </div >
-  </div >
+  return (
+    <div className="flex flex-col gap-4 items-center">
+      <div className="grid place-items-center h-[80vh]">
+        <BusinessCard />
+      </div>
+    </div>
+  )
 }
 
 export default VirtualBusinessCard;
---
+++ src/components/display/card-contact/BusinessCard.tsx
@@ -0,0 +1,52 @@
+import GlassCard from "@/components/containers/GlassCard";
+import SepartatorLine from "@/components/decorations/SeparatorLine";
+import Location from "@/components/display/information/Location";
+import { EnvelopeIcon } from "@heroicons/react/20/solid";
+
+import Image from 'next/image'
+
+type BusinessCardProps = {
+  hideContact?: boolean;
+}
+
+const BusinessCard = ({ hideContact }: BusinessCardProps) => {
+  return (
+    <div className="w-fit">
+      <GlassCard>
+        <div className="min-w-[22rem] flex flex-col items-center p-4 gap-4">
+          <div className="h-50 aspect-square bg-stone-700 grid place-items-center overflow-hidden rounded-full">
+            <Image src="/img/photos/profile_img_square.png" alt="photo of rei armenia" width={200} height={200} />
+          </div>
+          <div className="flex flex-col items-center gap-1">
+            <div className="flex flex-row items-center gap-2">
+              <div className="text-xl font-sans font-semibold text-white">Rei Armenia</div>
+              <div className="italic text-sm text-white/40">{'(they/them)'}</div>
+            </div>
+            <Location location={'Capital Region, New York, USA'} />
+          </div>
+          {
+            !hideContact && (<>
+              <SepartatorLine />
+              <div className="flex flex-col px-4 gap-2">
+                <a className="flex flex-row gap-4 items-center opacity-80" href="https://www.linkedin.com/in/rarmenia/" target="_blank">
+                  <Image src="/img/logos/linked_in.png" width="24" height="24" alt="linked in logo" className="brightness-[10]" />
+                  <span className="font-mono">/in/rarmenia</span>
+                </a>
+                <a className="flex flex-row gap-4 items-center opacity-80" href="https://github.com/rarmenia" target="_blank">
+                  <Image src="/img/logos/github.png" width="24" height="24" alt="linked in logo" className="brightness-[10]" />
+                  <span className="font-mono">rarmenia</span>
+                </a>
+                <a className="flex flex-row gap-4 items-center opacity-80" href="mailto:rei.armenia@gmail.com" target="_blank">
+                  <EnvelopeIcon className="h-6 text-white" />
+                  <span className="font-mono">rei.armenia@gmail</span>
+                </a>
+              </div>
+            </>)
+          }
+        </div>
+      </GlassCard>
+    </div >
+  )
+}
+
+export default BusinessCard;
