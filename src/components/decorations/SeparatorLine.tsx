047eb27e4c3afd79781e5b8f4c1ca167cddeab0f
Rei Armenia <5091445+rarmenia@users.noreply.github.com>
2023-08-13 21:17:35 -0400

    finish grid

---
+++ src/components/decorations/SeparatorLine.tsx
@@ -0,0 +1,9 @@
+const SepartatorLine = () => {
+
+  return (
+    <div className="w-full h-[1px] bg-gradient-to-l from-white/0 via-white/30 to-white/0"></div>
+  )
+
+}
+
+export default SepartatorLine;
